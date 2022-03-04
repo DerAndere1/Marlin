565c94b772d20c551870ac5408027ee7cea24bbb
DerAndere <26200979+DerAndere1@users.noreply.github.com>
2022-03-04 04:58:27 +0100

    Add LCD_SHOW_SECONDARY_AXES

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1375,6 +1375,9 @@
   // Show the E position (filament used) during printing
   //#define LCD_SHOW_E_TOTAL
 
+  // Show the position of secondary axes I[J[K]] instead of icons. Incompatible with LCD_SHOW_SECONDARY_AXES
+  //#define LCD_SHOW_SECONDARY_AXES
+
   /**
    * LED Control Menu
    * Add LED Control to the LCD menu
