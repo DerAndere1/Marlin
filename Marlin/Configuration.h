56617d6f267704b9218a18b8d214edb12ee35efc
DerAndere <26200979+DerAndere1@users.noreply.github.com>
2022-06-16 12:18:13 +0200

    Customize Version.h and bootscreen

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -88,7 +88,7 @@
  */
 
 // Show the Marlin bootscreen on startup. ** ENABLE FOR PRODUCTION **
-//#define SHOW_BOOTSCREEN
+#define SHOW_BOOTSCREEN
 
 // Show the bitmap in Marlin/_Bootscreen.h on startup.
 //#define SHOW_CUSTOM_BOOTSCREEN
@@ -147,7 +147,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-//#define CUSTOM_MACHINE_NAME "PipetBot-A8"
+#define CUSTOM_MACHINE_NAME "PipetBot-A8"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
