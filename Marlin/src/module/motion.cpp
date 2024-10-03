974f35d14cb4abd98010cdb612b95ad6213778ef
DerAndere <26200979+DerAndere1@users.noreply.github.com>
2024-10-03 12:39:53 +0200

    Add G43, G43.4 and G49

--- Marlin/src/module/motion.cpp
+++ Marlin/src/module/motion.cpp
@@ -218,7 +218,7 @@ xyz_pos_t cartes;
 
 
   #if ANY(PENTA_AXIS_TRT, PENTA_AXIS_HT)
-    bool tool_centerpoint_control = true;
+    bool tool_centerpoint_control = false;
   #endif
 
 #endif // IS_KINEMATIC
--- Marlin/src/module/tool_change.cpp
+++ Marlin/src/module/tool_change.cpp
@@ -1378,7 +1378,13 @@ void tool_change(const uint8_t new_tool, bool no_move/*=false*/) {
       #endif
 
       #if HAS_HOTEND_OFFSET
-        xyz_pos_t diff = motion.hotend_offset[new_tool] - motion.hotend_offset[old_tool];
+        #if HAS_TOOL_LENGTH_COMPENSATION
+          xyz_pos_t diff{0};
+          if (motion.simple_tool_length_compensation || motion.tool_centerpoint_control)
+            diff = motion.hotend_offset[new_tool] - motion.hotend_offset[old_tool];
+        #else
+          const xyz_pos_t diff = motion.hotend_offset[new_tool] - motion.hotend_offset[old_tool];
+        #endif
         TERN_(DUAL_X_CARRIAGE, diff.x = 0);
       #else
         constexpr xyz_pos_t diff{0};
