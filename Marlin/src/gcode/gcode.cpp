d75a191f0a7db8a6c25349384ee75c2b4b39734c
DerAndere <26200979+DerAndere1@users.noreply.github.com>
2025-08-05 00:15:21 +0200

    FEEDRATE_MODE_SUPPORT. Adds G93 inverse time mode and G94 unis per minute mode

--- Marlin/src/gcode/gcode.cpp
+++ Marlin/src/gcode/gcode.cpp
@@ -82,6 +82,11 @@ millis_t GcodeSuite::previous_move_ms = 0,
 // Relative motion mode for each logical axis
 relative_t GcodeSuite::axis_relative; // Init in constructor
 
+#if ENABLED(FEEDRATE_MODE_SUPPORT)
+  bool GcodeSuite::inverse_time_enabled = false;
+#endif
+
+
 #if ANY(HAS_AUTO_REPORTING, HOST_KEEPALIVE_FEATURE)
   bool GcodeSuite::autoreport_paused; // = false
 #endif
@@ -201,7 +206,11 @@ void GcodeSuite::get_destination_from_command() {
   #endif
 
   if (parser.floatval('F') > 0) {
-    const float fr_mm_min = parser.value_linear_units();
+    #if ENABLED(FEEDRATE_MODE_SUPPORT)
+      const float fr_mm_min = inverse_time_enabled ? parser.value_float() : parser.value_linear_units();
+    #else
+      const float fr_mm_min = parser.value_linear_units();
+    #endif
     feedrate_mm_s = MMM_TO_MMS(fr_mm_min);
     // Update the cutter feed rate for use by M4 I set inline moves.
     TERN_(LASER_FEATURE, cutter.feedrate_mm_m = fr_mm_min);
@@ -462,6 +471,11 @@ void GcodeSuite::process_parsed_command(bool no_ok/*=false*/) {
       case 91: G91(); break;                                      // G91: Relative Mode
 
       case 92: G92(); break;                                      // G92: Set current axis position(s)
+
+      #if ENABLED(FEEDRATE_MODE_SUPPORT)
+        case 93: G93(); break;                                  // G93: Set feedrate mode to inverse time
+        case 94: G94(); break;                                  // G94: Set feedrate mode to length units per minute
+      #endif
 
       #if ENABLED(CALIBRATION_GCODE)
         case 425: G425(); break;                                  // G425: Perform calibration with calibration cube
--- Marlin/src/module/planner.cpp
+++ Marlin/src/module/planner.cpp
@@ -2225,17 +2225,24 @@ bool Planner::_populate_block(
    * EXAMPLE: At 120mm/s a 60mm move involving XYZ axes takes 0.5s. So this will give 2.0.
    * EXAMPLE: At 120°/s a 60° move involving only rotational axes takes 0.5s. So this will give 2.0.
    */
-  float inverse_secs = inverse_millimeters * (
-    #if ALL(HAS_ROTATIONAL_AXES, INCH_MODE_SUPPORT)
-      /**
-       * Workaround for premature feedrate conversion
-       * from in/s to mm/s by get_distance_from_command.
-       */
-      cartesian_move ? fr_mm_s : LINEAR_UNIT(fr_mm_s)
-    #else
-      fr_mm_s
-    #endif
-  );
+
+  float inverse_secs;
+  if (TERN0(FEEDRATE_MODE_SUPPORT, inverse_time_enabled)) {
+    inverse_secs = fr_mm_s;
+  }
+  else {
+    inverse_secs  = inverse_millimeters * (
+      #if ALL(HAS_ROTATIONAL_AXES, INCH_MODE_SUPPORT)
+        /**
+         * Workaround for premature feedrate conversion
+         * from in/s to mm/s by get_distance_from_command.
+         */
+        cartesian_move ? fr_mm_s : LINEAR_UNIT(fr_mm_s)
+      #else
+        fr_mm_s
+      #endif
+    );
+  }
 
   // Get the number of non busy movements in queue (non busy means that they can be altered)
   const uint8_t moves_queued = nonbusy_movesplanned();
