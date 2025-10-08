3d62c4490b1f4114b95f590784d169c756f13490
DerAndere <26200979+DerAndere1@users.noreply.github.com>
2025-10-09 01:43:50 +0200

    fix inverse time mode

--- Marlin/src/module/motion.cpp
+++ Marlin/src/module/motion.cpp
@@ -161,6 +161,7 @@ xyze_pos_t destination; // {0}
   #define DEFAULT_FEEDRATE_MM_M 4000
 #endif
 feedRate_t feedrate_mm_s = MMM_TO_MMS(DEFAULT_FEEDRATE_MM_M);
+bool print_move = false;
 int16_t feedrate_percentage = 100;
 
 // Cartesian conversion result goes here:
