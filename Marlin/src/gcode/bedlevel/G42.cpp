4a4ae5298d5f065a3efb953d4ebf743edf3979da
DerAndere <26200979+DerAndere1@users.noreply.github.com>
2025-12-16 20:57:19 +0100

    fixup2

--- Marlin/src/gcode/bedlevel/G42.cpp
+++ Marlin/src/gcode/bedlevel/G42.cpp
@@ -53,6 +53,10 @@ void GcodeSuite::G42() {
     SERIAL_ECHOLNPGM(STR_ERR_MESH_XY);
     return;
   }
+  #if HAS_ROTATIONAL_AXES || IS_KINEAMETIC || HAS_LEVELING || ENABLED(FEEDRATE_MODE_SUPPORT)
+    parser.linear_motion_gcode = true;
+  #endif
+
 
   // Move to current_position, as modified by I, J, P parameters
   destination = current_position;
@@ -76,6 +80,11 @@ void GcodeSuite::G42() {
   #else
     prepare_internal_move_to_destination(fr_mm_s);
   #endif
+  #if HAS_ROTATIONAL_AXES || IS_KINEAMETIC || HAS_LEVELING || ENABLED(FEEDRATE_MODE_SUPPORT)
+    parser.linear_motion_gcode = false;
+  #endif
+  
+  }
 }
 
 #endif // HAS_MESH
