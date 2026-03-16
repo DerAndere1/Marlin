729f3ee4887046cf0cd203fa316e681a3ed6231a
DerAndere <26200979+DerAndere1@users.noreply.github.com>
2026-03-16 03:55:36 +0100

    fiixup feedrate mode

--- Marlin/src/module/motion.cpp
+++ Marlin/src/module/motion.cpp
@@ -1876,8 +1876,9 @@ float Motion::get_move_distance(const xyze_pos_t &diff OPTARG(HAS_ROTATIONAL_AXE
         return;
       }
       #if HAS_ROTATIONAL_AXES
-        cartesian_move = true;
+        bool cartesian_move = true;
       #endif
+      float cartesian_mm;
       // Get the move distance
       if (!parser.linear_motion_gcode)
         float cartesian_mm = get_move_distance(diff OPTARG(HAS_ROTATIONAL_AXES, cartesian_move));
