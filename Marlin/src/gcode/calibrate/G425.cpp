e1353f424a53e46aae22388d00d06f5a574f6aa6
DerAndere <26200979+DerAndere1@users.noreply.github.com>
2026-07-08 02:32:15 +0200

    Make G425 work with G49

--- Marlin/src/gcode/calibrate/G425.cpp
+++ Marlin/src/gcode/calibrate/G425.cpp
@@ -516,8 +516,10 @@ inline void calibrate_backlash(measurements_t &m, const float uncertainty) {
 }
 
 inline void update_measurements(measurements_t &m, const AxisEnum axis) {
-  motion.position[axis] += m.pos_error[axis];
-  m.obj_center[axis] = motion.calibration_center[axis];
+  if (TERN1(HAS_TOOL_CENTERPOINT_CONTROL, motion.tool_centerpoint_control) || TERN1(HAS_TOOL_LENGTH_COMPENSATION, motion.simple_tool_length_compensation)) {
+    motion.position[axis] += m.pos_error[axis];
+    m.obj_center[axis] = motion.calibration_center[axis];
+  }
   m.pos_error[axis] = 0;
 }
 
@@ -543,7 +545,7 @@ inline void calibrate_toolhead(measurements_t &m, const float uncertainty, const
 
   // Adjust the hotend offset
   #if HAS_HOTEND_OFFSET
-    xyz_pos_t &hotoff = motion.active_hotend_offset();
+    xyz_pos_t &hotoff = (TERN1(motion.tool_centerpoint_control) || TERN1(HAS_TOOL_LENGTH_COMPENSATION, motion.simple_tool_length_compensation)) ? motion.active_hotend_offset() : NUM_AXIS_ARRAY_1(0.0f);
     if (ENABLED(HAS_X_CENTER) && AXIS_CAN_CALIBRATE(X)) hotoff.x += m.pos_error.x;
     if (ENABLED(HAS_Y_CENTER) && AXIS_CAN_CALIBRATE(Y)) hotoff.y += m.pos_error.y;
                              if (AXIS_CAN_CALIBRATE(Z)) hotoff.z += m.pos_error.z;
