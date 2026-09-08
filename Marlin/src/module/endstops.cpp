7e374134c7f5baf71116ef5fe7380e205190b714
DerAndere <26200979+DerAndere1@users.noreply.github.com>
2026-09-09 01:52:50 +0200

    fixup3

--- Marlin/src/module/endstops.cpp
+++ Marlin/src/module/endstops.cpp
@@ -383,11 +383,11 @@ void Endstops::event_handler() {
     #else
         #define _ENDSTOP_SET_POS(A) do { \
         if (TERN1(G38_PROBE_TARGET, !G38_move.type) && TERN1(CALIBRATION_GCODE, !calibration_probe_enabled) && TERN0(HAS_##A##_MIN_STATE, TEST(hit_state, ES_ENUM(A,MIN)))) { \
-          motion.position[_AXIS(A)] = motion.base_min_pos(_AXIS(A)) + (TERN0(HAS_TOOL_CENTERPOINT_CONTROL, motion.tool_centerpoint_control) || TERN1(HAS_TOOL_LENGTH_COMPENSATION, motion.simple_tool_length_compensation)) ? motion.hotend_offset[motion.extruder][_AXIS(A)] : 0.0f; \
+          motion.position[_AXIS(A)] = motion.base_min_pos(_AXIS(A)) + TERN0(HAS_HOME_OFFSET, motion.home_offset[_AXIS(A)]) + (TERN0(HAS_TOOL_CENTERPOINT_CONTROL, motion.tool_centerpoint_control) || TERN1(HAS_TOOL_LENGTH_COMPENSATION, motion.simple_tool_length_compensation)) ? motion.hotend_offset[motion.extruder][_AXIS(A)] : 0.0f; \
           motion.destination[_AXIS(A)] = motion.position[_AXIS(A)]; \
         } \
         if (TERN0(HAS_##A##_MAX_STATE, TEST(hit_state, ES_ENUM(A,MAX)))) { \
-          motion.position[_AXIS(A)] = motion.base_max_pos(_AXIS(A)) + (TERN0(HAS_TOOL_CENTERPOINT_CONTROL, motion.tool_centerpoint_control) || TERN1(HAS_TOOL_LENGTH_COMPENSATION, motion.simple_tool_length_compensation)) ? motion.hotend_offset[motion.extruder][_AXIS(A)] : 0.0f; \
+          motion.position[_AXIS(A)] = motion.base_max_pos(_AXIS(A)) + TERN0(HAS_HOME_OFFSET, motion.home_offset[_AXIS(A)]) + (TERN0(HAS_TOOL_CENTERPOINT_CONTROL, motion.tool_centerpoint_control) || TERN1(HAS_TOOL_LENGTH_COMPENSATION, motion.simple_tool_length_compensation)) ? motion.hotend_offset[motion.extruder][_AXIS(A)] : 0.0f; \
           motion.destination[_AXIS(A)] = motion.position[_AXIS(A)]; \
         } \
       } while(0);  
@@ -396,18 +396,20 @@ void Endstops::event_handler() {
     if (abort_enabled()) {
 
       MAIN_AXIS_MAP(_ENDSTOP_SET_POS);
-
-      if (TERN0(HAS_Z_MAX_STATE, TEST(hit_state, ES_ENUM(Z,MAX)))){
-        if (TERN0(HAS_TOOL_CENTERPOINT_CONTROL, motion.tool_centerpoint_control) || TERN1(HAS_TOOL_LENGTH_COMPENSATION, motion.simple_tool_length_compensation)) {
-          motion.position.z = (Z_MAX_POS) + motion.hotend_offset[motion.extruder].z;
+      #if HAS_Z_AXIS
+        if (TERN0(HAS_Z_MAX_STATE, TEST(hit_state, ES_ENUM(Z,MAX)))){
+          if (TERN0(HAS_TOOL_CENTERPOINT_CONTROL, motion.tool_centerpoint_control) || TERN1(HAS_TOOL_LENGTH_COMPENSATION, motion.simple_tool_length_compensation)) {
+            motion.position.z = (Z_MAX_POS) + TERN0(HAS_HOME_OFFSET, motion.home_offset.z) + motion.hotend_offset[motion.extruder].z;
+          }
+          else {
+            motion.position.z = (Z_MAX_POS) + TERN0(HAS_HOME_OFFSET, motion.home_offset.z);
+          }
+          motion.destination.z = motion.position.z;
         }
-        else {
-          motion.position.z = (Z_MAX_POS);
-        }
-        motion.destination.z = motion.position.z;
-      }
-      SERIAL_ECHOLNPGM("hoff_z: ",motion.hotend_offset[motion.extruder].z);
-      SERIAL_ECHOLNPGM("z: ", motion.position.z);
+        SERIAL_ECHOLNPGM("hoff_z: ",motion.hotend_offset[motion.extruder].z);
+        SERIAL_ECHOLNPGM("base_max_z: ",motion.base_max_pos(Z_AXIS);
+        SERIAL_ECHOLNPGM("z: ", motion.position.z);
+      #endif
       motion.sync_plan_position();
     }
   
