e1a64febcdff85e9323d8c0ac7bac9940d3297cd
DerAndere <26200979+DerAndere1@users.noreply.github.com>
2026-09-08 03:34:49 +0200

    No position sync to Z min during probing or G425

--- Marlin/src/module/endstops.cpp
+++ Marlin/src/module/endstops.cpp
@@ -369,38 +369,21 @@ void Endstops::event_handler() {
       );
     #endif
 
-    #define _AX_ENUM(A) A##_AXIS
-    #define AX_ENUM(A) _AX_ENUM(A)
-
-    #define _AX_MAX_EHS(A) A##_MAX_ENDSTOP_HIT_STATE
-    #define AX_MAX_EHS(A) _AX_MAX_EHS(A)
-
-    #define _AX_MIN_EHS(A) A##_MIN_ENDSTOP_HIT_STATE
-    #define AX_MIN_EHS(A) _AX_MIN_EHS(A)
-
-    #define ENDSTOP_SET_POS(A) do { \
-      if (TERN0(HAS_##A##_MIN_STATE, TEST(hit_state, ES_ENUM(A,MIN)))) { \
-        motion.position[AX_ENUM(A)] = motion.base_min_pos(AX_ENUM(A)); \
-        motion.destination[AX_ENUM(A)] = motion.position[AX_ENUM(A)];  \
+    #define _ENDSTOP_SET_POS(A) do { \
+      if (TERN1(HAS_BED_PROBE, !z_probe_enabled) && TERN1(G38_PROBE_TARGET, !G38_move.type) && TERN1(CALIBRATION_GCODE, !calibration_probe_enabled) && TERN0(HAS_##A##_MIN_STATE, TEST(hit_state, ES_ENUM(A,MIN)))) { \
+        motion.position[AX_ENUM(A)] = motion.base_min_pos(_AXIS(A)) + (TERN0(HAS_TOOL_CENTERPOINT_CONTROL, motion.tool_centerpoint_control) || TERN1(HAS_TOOL_LENGTH_COMPENSATION, motion.simple_tool_length_compensation)) ? motion.hotend_offset[motion.extruder][_AXIS(A)] : 0.0f; \
+        motion.destination[AX_ENUM(A)] = motion.position[AXIS(A)]; \
       } \
       if (TERN0(HAS_##A##_MAX_STATE, TEST(hit_state, ES_ENUM(A,MAX)))) { \
-        motion.position[AX_ENUM(A)] = motion.base_max_pos(AX_ENUM(A)) + (TERN0(HAS_TOOL_CENTERPOINT_CONTROL, motion.tool_centerpoint_control) || TERN1(HAS_TOOL_LENGTH_COMPENSATION, motion.simple_tool_length_compensation)) ? motion.hotend_offset[motion.extruder][AX_ENUM(A)] : 0.0f; \
-        motion.destination[AX_ENUM(A)] = motion.position[AX_ENUM(A)]; \
+        motion.position[AX_ENUM(A)] = motion.base_max_pos(_AXIS(A)) + (TERN0(HAS_TOOL_CENTERPOINT_CONTROL, motion.tool_centerpoint_control) || TERN1(HAS_TOOL_LENGTH_COMPENSATION, motion.simple_tool_length_compensation)) ? motion.hotend_offset[motion.extruder][_AXIS(A)] : 0.0f; \
+        motion.destination[AX_ENUM(A)] = motion.position[_AXIS(A)]; \
       } \
     } while(0);
 
     if (abort_enabled()) {
-      NUM_AXIS_CODE(
-        ENDSTOP_SET_POS(X),
-        ENDSTOP_SET_POS(Y),
-        ENDSTOP_SET_POS(Z),
-        ENDSTOP_SET_POS(I),
-        ENDSTOP_SET_POS(J),
-        ENDSTOP_SET_POS(K),
-        ENDSTOP_SET_POS(U),
-        ENDSTOP_SET_POS(V),
-        ENDSTOP_SET_POS(W)
-      );
+
+      LOGICAL_AXIS_MAP(_ENDSTOP_SET_POS);
+
       if (TERN0(HAS_Z_MAX_STATE, TEST(hit_state, ES_ENUM(Z,MAX)))){
         if (TERN0(HAS_TOOL_CENTERPOINT_CONTROL, motion.tool_centerpoint_control) || TERN1(HAS_TOOL_LENGTH_COMPENSATION, motion.simple_tool_length_compensation)) {
           motion.position.z = (Z_MAX_POS) + motion.hotend_offset[motion.extruder].z;
